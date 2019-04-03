aea405d7b0cdfeb44b7c82fe914f2740bb2c378e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 16:16:11 -0400

    Updates the res.send() message in the server.get() statement in the server.js file

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.send("The server for web-Auth-iv-guided! ...It's alive!");
+  res.send("The server for the web-auth-iv-guided project! ...It's alive!");
 });
 
 module.exports = server;
